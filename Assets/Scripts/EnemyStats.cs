581e7632279d67e5fdd4c3a290a302bb29179869
Bilal Itani <bilalitani1@gmail.com>
2017-07-31 08:11:09 -0400

    Add working hp bar

---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyStats : MonoBehaviour {
+
+    public Transform healthBar;
+    public Slider healthFill;
+
+    public float currentHealth;
+
+    public float maxHealth;
+
+    public float healthBarYOffset = 2;
+
+    private void Update()
+    {
+        PositionHealthBar();
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            ChangeHealth(-5);
+        }
+    }
+
+    public void ChangeHealth(int amount) 
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        healthFill.value = currentHealth / maxHealth;
+    }
+
+    private void PositionHealthBar() 
+    {
+        Vector3 currentPos = transform.position;
+
+        healthBar.position = new Vector3(currentPos.x, currentPos.y + healthBarYOffset, currentPos.z);
+
+        healthBar.LookAt(Camera.main.transform);
+    }
+
+}
